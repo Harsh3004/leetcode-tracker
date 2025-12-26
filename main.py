b3ab045288eca516eadf3aff6a55980b4fd9573b
Harsh Joshi <108462704+Harsh3004@users.noreply.github.com>
2025-12-26 16:45:32 +0000

    Automation code

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+
+LEETCODE_USERNAME = "Harsh_Joshi_"
+
+def get_leetcode_data():
+    url = "https://leetcode.com/graphql"
+    query = {
+        "query": """
+        query recentAcSubmissionList($username: String!) {
+          recentAcSubmissionList(username: $username, limit: 10) {
+            title
+            titleSlug
+            timestamp
+            lang
+          }
+        }
+        """,
+        "variables": {"username": LEETCODE_USERNAME}
+    }
+    return requests.post(url, json=query).json()["data"]["recentAcSubmissionList"]
+
+def update_sheet():
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "credentials.json",
+        ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+    )
+
+    client = gspread.authorize(creds)
+    sheet = client.open("LeetCode Tracker").sheet1
+
+    data = get_leetcode_data()
+    for item in data:
+        sheet.append_row([
+            datetime.fromtimestamp(int(item["timestamp"])).strftime("%Y-%m-%d"),
+            item["title"],
+            item["lang"],
+            f"https://leetcode.com/problems/{item['titleSlug']}/"
+        ])
+
+if __name__ == "__main__":
+    update_sheet()
